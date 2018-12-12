f978055a59241eb82510785aaaca53520fb6a101
Janine Luk <janineluk95@gmail.com>
2018-12-12 17:24:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end 
+  
+  post '/puppy' do 
+    @puppy = Puppy.new(params[:name], params[:age], params[:breed])
+    @name = @puppy.name
+    @age = @puppy.age
+    @breed = @puppy.breed 
+    erb :display_puppy
+  end
+
 end
